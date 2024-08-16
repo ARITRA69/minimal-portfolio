8cf78e724c697c49654bc2572e7bb7eb44fd48f4
Aritra Sarkar <aritrasarkar2002@gmail.com>
2024-08-17 02:49:29 +0530

    contact form grid responsive done

--- components/shared/contact.tsx
+++ components/shared/contact.tsx
@@ -7,7 +7,7 @@ export const Contact = () => {
   return (
     <div id="contact" className="flex flex-col gap-4">
       <h5 className={cn(bricolageGrotesque, "text-xl font-bold")}>Contact</h5>
-      <div className="grid grid-cols-2 gap-4">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
         <div className="border border-foreground/10 px-4 py-2 flex items-center gap-2">
           <MapPin size={16} />
           <p className="text-sm opacity-70">Kolkata, India</p>

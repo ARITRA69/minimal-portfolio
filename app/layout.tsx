a9a95c520ccb85b3cc46e74760cee68832e4b212
Aritra Sarkar <aritrasarkar2002@gmail.com>
2024-08-25 03:02:57 +0530

    microsoft clarity enabled

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
+import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Inter } from "next/font/google";
+import { Toaster } from "react-hot-toast";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme/theme-provider";
 import { NavigationBar } from "@/components/shared/navigation-bar";
-import { Analytics } from "@vercel/analytics/react";
-import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -30,6 +31,15 @@ export default function RootLayout({
           {children}
           <NavigationBar />
           <Analytics />
+          <Script id="microsoft-clarity" strategy="afterInteractive">
+            {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "nshgjpx3dk");
+          `}
+          </Script>
           <Toaster />
         </ThemeProvider>
       </body>

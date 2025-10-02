2ce8dd25fa1897105becc0262c5f7e6a9df3825a
aritra-sudo <aritra@prodgain.com>
2025-10-02 11:47:53 +0530

    Add download button for resume and include PDF file

--- components/shared/hero.tsx
+++ components/shared/hero.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import { bricolageGrotesque } from "@/fonts";
 import { cn } from "@/lib/utils";
-import { CodeXml, Minus } from "lucide-react";
+import { CodeXml, Download, Minus } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 export const Hero = () => {
   return (
@@ -21,6 +24,21 @@ export const Hero = () => {
           I think about every detail in my work - from idea and analytics to
           prototype and visual design.
         </p>
+        <Button
+          size="sm"
+          variant="link"
+          className="w-max mt-2 gap-2 flex items-center"
+          onClick={() => {
+            const link = document.createElement("a");
+            link.href = "/Aritra Sarkar Resume A4.pdf";
+            link.download = "Aritra Sarkar Resume.pdf";
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+          }}
+        >
+          <Download size={16} /> <span>Resume</span>
+        </Button>
       </div>
     </div>
   );
